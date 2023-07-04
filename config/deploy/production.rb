1515226d4dcee690417ed7d48b50e7ae84595250
poolstack <deepak.j@poolstack.in>
2023-07-04 11:39:04 +0530

    change production file

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,7 +60,7 @@
 #     # password: "please use keys"
 #   }
 
-set :stage, :production
-set :rails_env, :production
-set :branch, "master"
-server "13.234.136.139", user: "ubuntu", roles: %w{web app db}
+# set :stage, :production
+# set :rails_env, :production
+# set :branch, "master"
+# server "13.234.136.139", user: "ubuntu", roles: %w{web app db}
