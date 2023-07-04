d1db30ff9e2fff78e21f6e15e60920a1cc096e05
poolstack <deepak.j@poolstack.in>
2023-07-04 13:54:36 +0530

    change master to main

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "vietus"
 set :repo_url, "git@github.com:poolstack/vietus.git"
 # restart app by running: touch tmp/restart.txt
 # at server machine
-set :user,'ubuntu'
+set :user, 'ubuntu'
 
 set :passenger_restart_with_touch, true
 set :rails_env, :development
@@ -23,46 +23,51 @@ set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log, "#{release_path}/log/puma.access.log"
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, false 
-set :deploy_to,"/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_init_active_record, false
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 
-#set :deploy_to, "/var/www/vietus"
+# set :deploy_to, "/var/www/vietus"
 namespace :puma do
- desc "Create Directories for Puma Pids and Socket"
- task :make_dirs do
- on roles(:app) do
- execute "mkdir #{shared_path}/tmp/sockets -p"
- execute "mkdir #{shared_path}/tmp/pids -p"
- end
- end
-before :start, :make_dirs
+  desc "Create Directories for Puma Pids and Socket"
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
 end
+
 namespace :deploy do
- desc "Make sure local git is in sync with remote."
- task :check_revision do
- on roles(:app) do
- unless 'git rev-parse HEAD` == `git rev-parse origin/master'
- puts "WARNING: HEAD is not the same as origin/master"
- puts "Run `git push` to sync changes."
- exit
- end
- end
- end
-desc "Initial Deploy"
- task :initial do
- on roles(:app) do
- before "deploy:restart", "puma:start"
- invoke "deploy"
- end
- end
-desc "Restart application"
- task :restart do
- on roles(:app), in: :sequence, wait: 5 do
- invoke “puma:restart”
- end
- end
-before :starting, :check_revision
- after :finishing, :compile_assets
- after :finishing, :cleanup
- after :finishing, :restart
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless 'git rev-parse HEAD' == 'git rev-parse origin/main'
+        puts "WARNING: HEAD is not the same as origin/main"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc "Initial Deploy"
+  task :initial do
+    on roles(:app) do
+      before "deploy:restart", "puma:start"
+      invoke "deploy"
+    end
+  end
+
+  desc "Restart application"
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke "puma:restart"
+    end
+  end
+
+  before :starting, :check_revision
+  after :finishing, :compile_assets
+  after :finishing, :cleanup
+  after :finishing, :restart
 end
